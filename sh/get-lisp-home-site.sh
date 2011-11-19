712f0529dd2fc19b64b9d9b31349f67035e64a17
Sergey Katrevich <linkfly@birulevo.net>
2011-11-19 06:29:20 +0400

    Added getting home site

---
+++ sh/get-lisp-home-site.sh
@@ -0,0 +1,9 @@
+#!/bin/sh
+
+##### Include scripts #####
+. ./includes.sh
+. ./core.sh
+
+local LISP_HOME_SITE_PARAM=$(uppercase $CUR_LISP)_HOME_SITE
+$(dirname $0)/get-default-param.sh $LISP_HOME_SITE_PARAM $(dirname $0)/conf/lisps.conf
+
