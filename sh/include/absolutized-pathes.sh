f250e34acb3e6d13109499f713a6a950f04427a3
Sergey Katrevich <linkfly@birulevo.net>
2011-11-30 15:53:50 +0400

    Adding quicklisp proceed

--- sh/include/absolutized-pathes.sh
+++ sh/include/absolutized-pathes.sh
@@ -4,6 +4,7 @@ abs_path SOURCES
 abs_path COMPILERS
 abs_path UTILS
 abs_path LISP_LIBS
+abs_path QUICKLISP
 abs_path EMACS_LIBS
 abs_path ARCHIVES
 abs_path TMP
--- sh/provide-quicklisp.sh
+++ sh/provide-quicklisp.sh
@@ -2,7 +2,21 @@
 cd $(dirname $0)
 . ./includes.sh
 
+if [ -f "$QUICKLISP/setup.lisp" ];then
+    echo "
+Quicklisp already installed in: $QUICKLISP
+
+ALREADY."; exit 0
+fi
+
 $SCRIPTS_DIR/provide-lisp.sh
-#$SCRIPTS_DIR/provide-wget.sh
-#./download-archive.sh http://beta.quicklisp.org/quicklisp.lisp $LISP_LIBS/quickload.lisp
-./run-lisp.sh --load $LISP_LIBS/quickload.lisp --eval \"\(quicklisp-quickstart:install :path \\\"$LISP_LIBS/quicklisp/\\\"\)\"
+mkdir --parents "$LISP_LIBS"
+$SCRIPTS_DIR/download-archive.sh "http://beta.quicklisp.org/quicklisp.lisp" "$LISP_LIBS/quickload.lisp"
+
+echo "(progn 
+        (print (truename \"$LISP_LIBS/quickload.lisp\"))
+        (load \"$LISP_LIBS/quickload.lisp\")
+        (funcall (find-symbol \"INSTALL\" :quicklisp-quickstart) :path \"$QUICKLISP/\")
+        (funcall (find-symbol \"ADD-TO-INIT-FILE\" :ql))
+        )" \
+ | ./run-lisp.sh
