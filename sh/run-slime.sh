aaa6df28b119231d54f5b23de1666e12032fab37
Sergey Katrevich <linkfly@birulevo.net>
2011-12-02 07:50:44 +0400

    Correct run-slime - automation to run Slime by run emacs

--- sh/run-slime.sh
+++ sh/run-slime.sh
@@ -10,4 +10,4 @@ Directory (where finded init-slime.el): $EMACS_LIBS
 FAILED.";exit 1;
 fi
 
-QUICKLISP="$QUICLISP" ./run-emacs.sh --load "$EMACS_LIBS/init-slime.el" "$@"
+QUICKLISP="$QUICLISP" ./run-emacs.sh --load "$EMACS_LIBS/init-slime.el" --eval "(slime)" "$@"
