d758493c70a70b7d56d255b1ade9e8b6a39b2a8e
Sergey Katrevich <linkfly@birulevo.net>
2011-12-02 16:19:42 +0400

    Corrected encoding for slime

--- emacs-libs/init-slime.el
+++ emacs-libs/init-slime.el
@@ -1,3 +1,7 @@
 
 (load (concat (getenv "QUICKLISP") "/slime-helper.el"))
+(if (string-equal (getenv "CUR_LISP") "clisp")
+  (load (concat (genenv "LISP_DIR") "/share/emacs/site-lisp/clisp-coding.el")))
+(setq slime-net-coding-system (quote utf-8-unix))
+(set-language-environment "utf-8")
 (setq inferior-lisp-program "run-lisp")
--- sh/provide-slime.sh
+++ sh/provide-slime.sh
@@ -5,8 +5,13 @@ cd $(dirname $0)
 ./provide-lisp.sh
 ./provide-emacs.sh
 ./provide-quicklisp.sh
+
 echo '(ql:quickload "quicklisp-slime-helper")' | ./run-lisp.sh
 echo '
 (load (concat (getenv "QUICKLISP") "/slime-helper.el"))
+(if (string-equal (getenv "CUR_LISP") "clisp")
+  (load (concat (genenv "LISP_DIR") "/share/emacs/site-lisp/clisp-coding.el")))
+(setq slime-net-coding-system (quote utf-8-unix))
+(set-language-environment "utf-8")
 (setq inferior-lisp-program "run-lisp")' \
 > $EMACS_LIBS/init-slime.el && echo "Line to run the SLIME copied into $EMACS_LIBS/init-slime.el"
