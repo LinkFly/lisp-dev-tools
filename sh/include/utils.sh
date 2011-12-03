ee7a5648ffe18ca82b06c779a6934cc5cb36f8b6
Sergey Katrevich <linkfly@birulevo.net>
2011-12-03 13:36:55 +0400

    smallfix

--- sh/include/utils.sh
+++ sh/include/utils.sh
@@ -418,8 +418,6 @@ rm -rf $TMP_TOOL_DIR
 mkdir --parents $TMP_TOOL_DIR
 cd $TMP_TOOL_DIR
 RESULT=1
-#echo "$(pwd)"
-#echo "$EXTRACT_SCRIPT $ARCHIVE_PATH && RESULT=0";exit 1
 $EXTRACT_SCRIPT $ARCHIVE_PATH && RESULT=0;
 
 if [ $RESULT = 1 ]; then
