1c8c02855b333cec4992185f6fd74954c5f2b7cb
Sergey Katrevich <linkfly@birulevo.net>
2012-01-08 02:51:50 +0400

    Else correct declare variables

--- sh/include/correct-params.sh
+++ sh/include/correct-params.sh
@@ -8,24 +8,24 @@ D=\$;
 
 if ! [ "$VERSION" = "" ];
 then
-local LISP_VERSION_PARAM=$(uppercase $CUR_LISP)_VERSION;
+LISP_VERSION_PARAM=$(uppercase $CUR_LISP)_VERSION;
 eval $LISP_VERSION_PARAM="$VERSION"
 fi
 
 if ! [ "$ARCH" = "" ];
 then
-local LISP_ARCH_PARAM=$(uppercase $CUR_LISP)_ARCH;
+LISP_ARCH_PARAM=$(uppercase $CUR_LISP)_ARCH;
 eval $LISP_ARCH_PARAM="$ARCH"
 fi
 
 if ! [ "$OS" = "" ];
 then
-local LISP_OS_PARAM=$(uppercase $CUR_LISP)_OS;
+LISP_OS_PARAM=$(uppercase $CUR_LISP)_OS;
 eval $LISP_OS_PARAM="$OS"
 fi
 
 if ! [ "$ENABLE_QUICKLISP" = "" ];
 then
-local LISP_OS_PARAM=$(uppercase $CUR_LISP)_ENABLE_QUICKLISP;
+LISP_OS_PARAM=$(uppercase $CUR_LISP)_ENABLE_QUICKLISP;
 eval $LISP_OS_PARAM="$ENABLE_QUICKLISP"
 fi
