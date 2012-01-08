b6d235de367d46edbccbda0a9a3641e1bb168e95
Sergey Katrevich <linkfly@birulevo.net>
2012-01-08 09:37:27 +0400

    Else removed local special word not into functions

--- sh/change-version.sh
+++ sh/change-version.sh
@@ -2,6 +2,6 @@
 cd $(dirname $0)
 . ./includes.sh
 
-local LISP_VERSION_PARAM=$(uppercase $CUR_LISP)_VERSION
+LISP_VERSION_PARAM=$(uppercase $CUR_LISP)_VERSION
 ./change-default-param.sh $LISP_VERSION_PARAM $1 $SCRIPTS_DIR/conf/lisps.conf && echo "Current lisp version: $(./get-default-param.sh $LISP_VERSION_PARAM)"
 
