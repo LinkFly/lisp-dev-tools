ed7c7d320ddb0ea9e6140e2699c65f737ed10d9a
Sergey Katrevich <linkfly@birulevo.net>
2012-01-08 07:35:49 +0400

    Corrected init-slime.el for case is not found slime-helper.el. Remove twice loaded init-slime.el

--- emacs-libs/init-slime.el
+++ emacs-libs/init-slime.el
@@ -1,7 +1,11 @@
-
-(load (concat (getenv "QUICKLISP") "/slime-helper.el"))
-(if (string-equal (getenv "CUR_LISP") "clisp")
-  (load (concat (genenv "LISP_DIR") "/share/emacs/site-lisp/clisp-coding.el")))
-(setq slime-net-coding-system (quote utf-8-unix))
-(set-language-environment "utf-8")
-(setq inferior-lisp-program "run-lisp")
+(let ((slime-helper (concat (getenv "QUICKLISP") "/slime-helper.el")))
+  (when (and slime-helper
+	     (not (boundp 'init-slime.el-loaded)))
+    (load slime-helper)
+    (if (string-equal (getenv "CUR_LISP") "clisp")
+	(load (concat (genenv "LISP_DIR") 
+		      "/share/emacs/site-lisp/clisp-coding.el")))
+    (setq slime-net-coding-system (quote utf-8-unix))
+    (set-language-environment "utf-8")
+    (setq inferior-lisp-program "run-lisp")
+    (setq init-slime.el-loaded t)))
--- sh/run-emacs.sh
+++ sh/run-emacs.sh
@@ -9,4 +9,5 @@ fi
 if [ -f $EMACS_LIBS/init-slime.el ];then
     LOAD_INIT="$LOAD_INIT --load $EMACS_LIBS/init-slime.el"
 fi
+
 PATH=$PREFIX:$PATH QUICKLISP=$QUICKLISP $UTILS/emacs --no-init-file --no-site-file${LOAD_INIT} "$@"
