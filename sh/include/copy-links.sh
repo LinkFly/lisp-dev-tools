17e7793c2d698b6e2dee4553b48c2c616126a229
Sergey Katrevich <linkfly@birulevo.net>
2012-04-16 23:54:47 +0400

    bugfix in providing links

--- sh/include/copy-links.sh
+++ sh/include/copy-links.sh
@@ -2,7 +2,7 @@
 
 if test -z "$(find "$UTILS" -maxdepth 1 -name "*" -type l 2>/dev/null)"
 then
-    cp -lf "$UTILS/reserv-default-links/*" "$UTILS/" 2>/dev/null
+    cp -lf "$UTILS/reserv-default-links/"* "$UTILS/" 2>/dev/null
     ln -sf internal-tools/rpm2cpio
 fi
 
