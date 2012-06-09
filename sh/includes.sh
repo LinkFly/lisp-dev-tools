45766a8ad56096f0516ef0ebf8bb455643510523
Sergey Katrevich <linkfly@birulevo.net>
2012-06-09 12:58:19 +0400

    Added to .gitignore custom files for configuring dirs and current lisp and it version. Added feature set dir where search custom-dis.conf and custom-lisp-and-version.conf by LDT_CUSTOM_CONF_DIR variable environment. Separated default current lisp and defaults current lisps-versions into lisp-and-version.conf

--- sh/includes.sh
+++ sh/includes.sh
@@ -1,13 +1,30 @@
 #!/bin/sh
+######
+# Note: includes when current-dir = <lisp-dev-tools>/sh
+######
+
+## get-prefix.sh must be into sh/get-prefix.sh
+PREFIX=$(./get-prefix.sh) 
+LDT_CUSTOM_CONF_DIR=${LDT_CUSTOM_CONF_DIR:-"$PREFIX/sh/conf"}
+
+include_if_exist () {
+if test -f "$1"
+then
+    . "$1"
+fi
+}
 
 ##### Include scripts #####
 . ./conf/dirs.conf
+include_if_exist "$LDT_CUSTOM_CONF_DIR/custom-dirs.conf"    
 . ./internal-conf/internal-dirs.conf
 . ./include/utils.sh
 . ./include/absolutized-pathes.sh
 . ./include/exit-if-lock.sh
 . ./include/copy-links.sh
 . ./conf/tools.conf
+. ./lisp-and-version.conf
+include_if_exist "$LDT_CUSTOM_CONF_DIR/custom-lisp-and-version.conf"    
 . ./conf/lisps.conf
 . ./include/correct-params.sh
 . ./internal-conf/internal-lisps.conf
